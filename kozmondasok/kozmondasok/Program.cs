d3a8bd78038a9ba6f61b33a1a5bdaf3ad4094592
Szabó István <pisti4395@gmail.com>
2025-01-31 07:35:35 +0100

    asdf

---
+++ kozmondasok/kozmondasok/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kozmondasok
+{
+    internal class Program
+    {
+        static List<string> egyesites(List<string> elso, List<string> masodik)
+        {
+            new StreamWriter("egyesitett.txt").Write(string.Join("\n", elso) +'\n'+ string.Join("\n", masodik));
+            return elso.Concat(masodik).ToList();
+        }
+        static List<string> kiir(List<string> egyesitett)
+        {
+            egyesitett.Sort();
+            foreach (string line in egyesitett)
+                Console.WriteLine(line);
+            return egyesitett;
+        }
+        static void nem
+        static void Main(string[] args)
+        {
+            var elso = (from x in new StreamReader("szoveg1.txt").ReadToEnd().Split('\n') select x.Trim()).ToList();
+            elso.RemoveAt(elso.Count - 1);
+            var masodik = (from x in new StreamReader("szoveg2.txt").ReadToEnd().Split('\n') select x.Trim()).ToList();
+            masodik.RemoveAt(masodik.Count - 1);
+            Console.WriteLine($"az elso file {elso.Count()} a masodik {masodik.Count()} sort tartalmaz");
+            kiir(egyesites(elso, masodik));
+
+            Console.ReadLine();
+        }
+    }
+}
--- kozmondasok/kozmondasok/Properties/AssemblyInfo.cs
+++ kozmondasok/kozmondasok/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SzI-szintabla")]
+[assembly: AssemblyTitle("kozmondasok")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SzI-szintabla")]
+[assembly: AssemblyProduct("kozmondasok")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a9cb8d3-3678-49ab-b4ff-83dd94593c16")]
+[assembly: Guid("a8a080f7-2610-4b84-b392-9a8fe0417d33")]
 
 // Version information for an assembly consists of the following four values:
 //
