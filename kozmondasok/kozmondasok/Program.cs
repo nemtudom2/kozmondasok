554398013352fc53f50cddc6ba964e9ce047777e
Szabó István <pisti4395@gmail.com>
2025-01-31 07:57:59 +0100

    kesz

--- kozmondasok/kozmondasok/Program.cs
+++ kozmondasok/kozmondasok/Program.cs
@@ -22,7 +22,20 @@ namespace kozmondasok
                 Console.WriteLine(line);
             return egyesitett;
         }
-        static void nem
+        static List<string> nemszokoz(List<string> egyesitett)
+        {
+            int nemszokoz = 0;
+            foreach (string line in egyesitett)
+                foreach (char letter in line)
+                    if (letter != ' ')
+                        nemszokoz++;
+            Console.WriteLine($"{nemszokoz} nem szokoz karakter van a sorokban");
+            return egyesitett;
+        }
+        static void mentes(List<string> egyesitett)
+        {
+            new StreamWriter("kozmondasok.txt").Write(string.Join("\n", egyesitett).ToString());
+        }
         static void Main(string[] args)
         {
             var elso = (from x in new StreamReader("szoveg1.txt").ReadToEnd().Split('\n') select x.Trim()).ToList();
@@ -30,7 +43,7 @@ namespace kozmondasok
             var masodik = (from x in new StreamReader("szoveg2.txt").ReadToEnd().Split('\n') select x.Trim()).ToList();
             masodik.RemoveAt(masodik.Count - 1);
             Console.WriteLine($"az elso file {elso.Count()} a masodik {masodik.Count()} sort tartalmaz");
-            kiir(egyesites(elso, masodik));
+            mentes(nemszokoz(kiir(egyesites(elso, masodik))));
 
             Console.ReadLine();
         }
